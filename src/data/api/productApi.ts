77b081acfefd96fcedca34e6bbf9fe36075b313d
Duvan Salazar <andres53271@hotmail.com>
2025-12-13 00:24:43 -0500

    fix:(api) fix API route

--- src/data/api/productApi.ts
+++ src/data/api/productApi.ts
@@ -2,7 +2,7 @@ import { axiosClient } from './axiosClient';
 
 
 export const getProducts = async () => {
-  const response = await axiosClient.get('product');
+  const response = await axiosClient.get('products');
   return response.data;
 }
 
