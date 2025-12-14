7082bc202697227c2c5588aa70b29e7d2be5b19c
Duvan Salazar <andres53271@hotmail.com>
2025-12-13 22:32:14 -0500

    feat:(screens) Created component to show message info

---
+++ src/presentation/components/EmptyState.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+interface Props {
+  text: string;
+}
+
+export const EmptyState = ({ text }: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '500',
+    textAlign: 'center',
+    color: '#888',
+  },
+});
--- src/presentation/screens/ProductsScreen.tsx
+++ src/presentation/screens/ProductsScreen.tsx
@@ -1,12 +1,18 @@
+import { useEffect } from 'react';
 import { FlatList, ActivityIndicator, Text, StyleSheet, View } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { useProducts } from '../hooks/useProduct';
 import { ProductCard } from '../components/ProductCard';
 import { RootStackParamList } from '../../app/navigation/RootStack';
+import { EmptyState } from '../components/EmptyState';
 
 export const ProductsScreen = () => {
   const { products, error, loading } = useProducts();
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  useEffect(() => {
+    navigation.setOptions({ title: 'Productos' });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -18,11 +24,11 @@ export const ProductsScreen = () => {
             onPress={() => navigation.navigate('ProductDetail', { id: item.id })}
           />
         )}
+        contentContainerStyle={{ flexGrow: 1 }}
         keyExtractor={item => item.id.toString()}
-        ListEmptyComponent={() => <Text style={{ textAlign: 'center', marginTop: 20, fontSize: 18 }}>No hay productos disponibles</Text>}
+        ListEmptyComponent={() => error && <EmptyState text="No hay productos disponibles" />}
       />
-      {error && <Text style={{ color: 'red', textAlign: 'center', marginTop: 20 }}>Error: {typeof error === 'object' ? JSON.stringify(error) : error}</Text>}
-      {loading && <ActivityIndicator size="large" color="#0000ff" style={{ position: 'absolute', top: '50%', left: '50%' }} />}
+      {loading && <ActivityIndicator size="large" color="#000000ff" style={{ position: 'absolute', top: '50%', left: '50%' }} />}
 
     </View>
   );
